315439d7245cb1018ab5bcc72cfebc8f7746b9cb
GASPARINI Nicolas <nicolas.gasp@free.fr>
2016-09-05 11:26:27 +0200

    first commit

---
+++ src/client.js
@@ -0,0 +1,77 @@
+const request       = require('request-promise');
+const winston       = require('winston');
+
+const Client = function client(config, customLogger) {
+  const instanceConfig = (typeof config === 'object') ? config : {};
+
+  instanceConfig.baseUrl = instanceConfig.baseUrl || process.env.API_URL;
+
+  this.logger  = customLogger || winston.loggers.add('eoko-client-api');
+  this.config  = instanceConfig;
+};
+
+Client.prototype.get = function getRequest(path) {
+  return this.request('GET', path)
+             .catch(err => {
+               this.logger.error(err);
+               throw err;
+             });
+};
+
+Client.prototype.post = function postRequest(path, body) {
+  return this.request('POST', path, body)
+             .catch(err => {
+               this.logger.error(err);
+               throw err;
+             });
+};
+
+Client.prototype.delete = function deleteRequest(path) {
+  return this.request('DELETE', path)
+             .catch(err => {
+               this.logger.error(err);
+               throw err;
+             });
+};
+
+Client.prototype.put = function putRequest(path, body) {
+  return this.request('PUT', path, body)
+             .catch(err => {
+               this.logger.error(err);
+               throw err;
+             });
+};
+
+Client.prototype.patch = function patchRequest(path, partialBody) {
+  return this.request('PATCH', path, partialBody)
+             .catch(err => {
+               this.logger.error(err);
+               throw err;
+             });
+};
+
+Client.prototype.request = function buildRequest(method, path, body) {
+  const options = {
+    method,
+    uri: this.config.baseUrl + path,
+    json: true,
+  };
+
+  if (process.env.API_KEY) {
+    options.headers = {
+      apikey: process.env.API_KEY,
+    };
+  }
+
+  if (method === 'POST' || method === 'PUT' || method === 'PATCH') {
+    options.body = body;
+  }
+
+  return request(options)
+    .catch(err => {
+      this.logger.error(err);
+      throw err;
+    });
+};
+
+module.exports = Client;
